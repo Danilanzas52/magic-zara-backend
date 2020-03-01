bbb3dc0bdfff0dfeecc87ef79ff67f97a4860996
Daniel Garcia Pulpeiro <daniel.garcia.pulpeiro@udc.es>
2020-03-01 23:27:06 +0100

    Implemented first aproach for text search

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,18 @@
 from flask import jsonify
+from database import MongoDB_Handler
+
 
 # Produce JSON from mongo docs
 def to_json(docs):
     list = [doc for doc in docs]
     return jsonify(list)
+
+
+def find_by_keywords(db, keyword_string):
+    #Todo: specify in readme that is mandatory to create a text index in mongodb
+    return db["zara"].find(
+        {
+            '$text': {
+                '$search': keyword_string,
+            }
+        })
