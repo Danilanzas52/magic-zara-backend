eaadfa4a9f54ccc852a9124b58ba380bda8ad0b6
Daniel Garcia Pulpeiro <daniel.garcia.pulpeiro@udc.es>
2020-03-01 23:29:21 +0100

    Merge branch 'develop' of https://github.com/Danilanzas52/magic-zara-backend into develop

--- src/main.py
+++ src/main.py
@@ -14,6 +14,7 @@ def get_all():
     category = request.args.get('category')
     query = {"category":category} if category else {}
     docs = db['exported_db_zara'].find(query, collection_filter)
+
     return utils.to_json(docs)
 
 @app.route('/<id>')
